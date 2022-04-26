47b4ab2c2cf2119cc83bfe2cc04e33934c8592da
Taha-boudiaf <husseintaha168@gmail.com>
2022-04-26 15:00:27 +0100

    install react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+     <Navbar/>
+    </>
   );
 }
 
