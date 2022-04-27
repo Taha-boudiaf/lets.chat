6a9d061cab93b7f61a2af04e83ce184151569cd8
Taha-boudiaf <husseintaha168@gmail.com>
2022-04-28 00:19:43 +0100

    add container and path page note found

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import { Container } from "@mui/material";
 import {BrowserRouter as Router,Routes ,Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import {HomePage,AboutPage,ContactPage} from './components/ListAsyncPage';
+import {HomePage,AboutPage,ContactPage, PageNoFound} from './components/ListAsyncPage';
 
 function App() {
   return (
     <Router>
       <Navbar/>
       <div className='App'>
-        <Routes>
+        <Container maxWidth="lg">
+          <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/about" element={<AboutPage/>}/>
           <Route path="/contact" element={<ContactPage/>}/>
+          <Route path="*" element={<PageNoFound/>}/>
         </Routes>
+        </Container>
       </div>
      </Router>
   );
