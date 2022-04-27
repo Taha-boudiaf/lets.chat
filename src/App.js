386b791daeb7cc197c28356771e3ddfb02af5200
Taha-boudiaf <husseintaha168@gmail.com>
2022-04-27 16:09:08 +0100

    use react router dom & create path home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import {BrowserRouter as Router,Routes ,Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Home from './view/Home';
 
 function App() {
   return (
-    <>
-     <Navbar/>
-    </>
+    <Router>
+      <Navbar/>
+      <div className='App'>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+     </Router>
   );
 }
 
