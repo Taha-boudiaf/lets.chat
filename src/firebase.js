9731180f08f022b8307cb8f62491643746ecf6eb
Taha-boudiaf <husseintaha168@gmail.com>
2022-04-30 21:38:16 +0100

    create firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0XzrQBdOHlNzl0gp0tH7MxKVev06pakg",
+  authDomain: "let-s-chat-81a27.firebaseapp.com",
+  projectId: "let-s-chat-81a27",
+  storageBucket: "let-s-chat-81a27.appspot.com",
+  messagingSenderId: "895023908304",
+  appId: "1:895023908304:web:8c5a2f0cf6fc2ccc65a5d5",
+  measurementId: "G-6H7N24ZW2J"
+};
+
+const app = initializeApp(firebaseConfig);
