e88d535bf658788ec757e4f0af390562dea433fa
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-02 15:51:21 +0100

    add register path

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import { Container } from "@mui/material";
 import {BrowserRouter as Router,Routes ,Route } from "react-router-dom";
-import './App.css';
+import {HomePage,AboutPage,ContactPage, PageNoFound,LoginPage,RegisterPage} from './components/ListAsyncPage';
+// import { UserAuthContext } from "./context/ChatContext";
 import Navbar from './components/layout/Navbar';
-import {HomePage,AboutPage,ContactPage, PageNoFound,LoginPage} from './components/ListAsyncPage';
 import Footer from './components/layout/Footer'
 
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar/>
-      <div className='App'>
-        <Container maxWidth="lg"> 
-          <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/about" element={<AboutPage/>}/>
-          <Route path="/contact" element={<ContactPage/>}/>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="*" element={<PageNoFound/>}/>
-        </Routes>
-        </Container> 
-      </div>
-      <Footer/>
-     </Router>
+    // <UserAuthContext> 
+      <Router>
+        <Navbar/>
+          <div className='App'>
+            <Container maxWidth="lg"> 
+              <Routes>
+              <Route path="/" element={<HomePage/>}/>
+              <Route path="/about" element={<AboutPage/>}/>
+              <Route path="/contact" element={<ContactPage/>}/>
+              <Route path="/login" element={<LoginPage/>}/>
+              <Route path="/register" element={<RegisterPage/>}/>
+              <Route path="*" element={<PageNoFound/>}/>
+            </Routes>
+            </Container> 
+          </div>
+        <Footer/>
+      </Router>
+    // {/* </UserAuthContext>   */}
   );
 }
 
