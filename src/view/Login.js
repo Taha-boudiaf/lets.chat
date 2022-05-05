e3387e130f1016745d7ca7b0fda565c4e0d2dee9
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-05 18:51:56 +0100

    create functionalty to login

--- src/view/Login.js
+++ src/view/Login.js
@@ -1,34 +1,75 @@
-import React from 'react'
-import { Container, Grid, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {  Grid, Paper, Typography } from '@mui/material'
 import { Form, Formik } from 'formik'
-import * as yup from 'yup'
-const INITIAL_FORM_STATE = {
+import {Validation} from '../components/FormUI/Yup/Yup'
+import styles from '../components/css/register.module.css'
+import { useAuth } from '../context/ChatContext'
+import Button from '../components/FormUI/Submit/index'
+import TextField from '../components/FormUI/Textfield/index'
+import { doc, updateDoc } from 'firebase/firestore'
+import { db } from '../firebase'
 
-}
-const FORM_VALIDATION = yup.object().shape({
-  firstName:yup.string().required('Required'),
-  LastName:yup.string().required('Required'),
-  email:yup.string().email('Invalid Email').required('Required'),
-})
+
+const FORM_VALIDATION = Validation;
+
+
+
 const Login = () => {
+  const [data,setData] = useState({
+    email:'',
+    password:'',
+  })
+  const {Login} = useAuth();
+  const Navigate = useNavigate();
+  const handleSubmit = async(data)=>{
+    setData({...data})
+    const res = await Login(data.email,data.password)
+    await updateDoc(doc(db,"users",res.user.uid),
+       {
+         isOnline:true,
+       });
+    Navigate('/')
+  }
   return (
-    <div>
-      <Grid container>
-        <Grid item xs={6}>
-          <Container>
-            <Formik initialValues={{...INITIAL_FORM_STATE}} validationSchema={FORM_VALIDATION} onSubmit={(values) =>{console.log(values);}}> 
+    <>
+      <Grid container spacing={4} className={styles.con} >
+      <Paper className={styles.Paper} >
+          <div>
+            <Typography variant="h3" style={{fontWeight: '600', fontSize: '2rem'}}>
+              Welcome Back!
+            </Typography>
+            <Typography variant="subtitle1" style={{marginBottom: '15px',lineHeight: '2.75',color:'#757982'}}>
+              Sign in to your account
+            </Typography>
+          </div>
+        <Grid item xs={12} >
+            <Formik initialValues={{...data}} validationSchema={FORM_VALIDATION} onSubmit={handleSubmit}> 
               <Form>
-                <Grid item xs={6}>
-                  <Typography>
-                    details
+                <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField className={styles.field} name='email' label='Email'/>
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField className={styles.field} name='password' label='Password' type='password'/>
+                </Grid>            
+                <Grid item xs={12}>
+                  <Button className={styles.btn}>
+                  Conect Now
+                  </Button>
+                </Grid> 
+                <div>
+                  <Typography variant="subtitle1" style={{marginLeft:'20px',marginBottom: '10px',lineHeight: '2.75',color:'#757982'}}>
+                    You Dont have Account? <a href='/register'>Join Now</a>
                   </Typography>
+                </div>
                 </Grid>
               </Form>
             </Formik>
-          </Container>
         </Grid>
-      </Grid>
-    </div>
+      </Paper>
+    </Grid>
+    </>
   )
 }
 
