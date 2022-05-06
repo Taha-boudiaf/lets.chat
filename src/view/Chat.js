e70f46a619b58e59d46d52e1f8aba26f3eb908d4
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-06 17:52:19 +0100

    change state of user online or offline

--- src/view/Chat.js
+++ src/view/Chat.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useAuth } from '../context/ChatContext';
+import { Button } from '@mui/material' 
+import { doc, updateDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import { db } from '../firebase';
 
 const Chat = () => {
+  const {Signout,user} = useAuth();
+  const Navigate = useNavigate();
+  const handleClick = async()=>{
+    await updateDoc(doc(db,"users",user.uid),{
+      isOnline:false
+      })
+      console.log(user.uid);
+      await Signout();    
+      Navigate('/')
+  }
   return (
-    <div>Chat</div>
+    <div>
+      <Button onClick={handleClick}>logout </Button>
+    </div>
   )
 }
 
