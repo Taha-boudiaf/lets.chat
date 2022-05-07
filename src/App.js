447706e7f029e60d86ec93534005251dad9c0e96
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-07 17:26:35 +0100

    use protected route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Container } from "@mui/material";
 import {BrowserRouter as Router,Routes ,Route } from "react-router-dom";
 import {HomePage,AboutPage,ContactPage, PageNoFound,LoginPage,RegisterPage,ChatPage} from './components/ListAsyncPage';
-import { UserAuthContext } from "./context/ChatContext";
+import {  UserAuthContext } from "./context/ChatContext";
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer'
-
+import ProtectedRoute from './components/ProtectedRoute'
 import './App.css';
 
 function App() {
+  
   return (
     <UserAuthContext> 
       <Router>
@@ -15,17 +16,17 @@ function App() {
           <div className='App'>
             <Container maxWidth="lg"> 
               <Routes>
-              <Route path="/" element={<HomePage/>}/>
-              <Route path="/about" element={<AboutPage/>}/>
-              <Route path="/contact" element={<ContactPage/>}/>
-              <Route path="/login" element={<LoginPage/>}/>
-              <Route path="/register" element={<RegisterPage/>}/>
-              <Route path="/chat" element={<ChatPage/>}/>
-              <Route path="*" element={<PageNoFound/>}/>
-            </Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/about" element={<AboutPage/>}/>
+                <Route path="/contact" element={<ContactPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/chat" element={<ProtectedRoute><ChatPage/></ProtectedRoute>}/>
+                <Route path="*" element={<PageNoFound/>}/>
+              </Routes>
             </Container> 
           </div>
-        <Footer/>
+          <Footer/>
       </Router>
     </UserAuthContext>  
   );
