d1bf84248a91f2198ebeeb93203010632a81dfcb
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-12 19:57:53 +0100

    create chat architacture

--- src/view/Chat.js
+++ src/view/Chat.js
@@ -1,26 +1,97 @@
-import React from 'react'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import { useAuth } from '../context/ChatContext';
-import { Button } from '@mui/material' 
-import { doc, updateDoc } from 'firebase/firestore';
-import { useNavigate } from 'react-router-dom';
-import { db } from '../firebase';
+import { Button } from '@mui/material';
+import SideBar from '../components/chat/SideBar';
+import MessageForm from '../components/chat/MessageForm';
 
-const Chat = () => {
-  const {Signout,user} = useAuth();
-  const Navigate = useNavigate();
-  const handleClick = async()=>{
-    await updateDoc(doc(db,"users",user.uid),{
-      isOnline:false
-      })
-      console.log(user.uid);
-      await Signout();    
-      Navigate('/')
+
+
+const drawerWidth = 240;
+
+function ResponsiveDrawer(props) {
+
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const {Signout} = useAuth()
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const handleSignout= () =>{
+    Signout()
   }
+  
+
+
   return (
-    <div>
-      <Button onClick={handleClick}>logout </Button>
-    </div>
-  )
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <SideBar/>
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+        <Typography paragraph>
+          Lorem 
+        </Typography>
+        <Typography paragraph>
+          Consequat 
+        </Typography>
+        <Button onClick={handleSignout}> Signout</Button>
+        <a href='/profile'>profile</a>
+      </Box>
+      <MessageForm
+                // handleSubmit= {handleSubmit}
+                // text={text}
+                // setText={setText}
+                // setImg={setImg}
+       />
+    </Box>
+  );
 }
 
-export default Chat
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default ResponsiveDrawer;
