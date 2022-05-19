2d200650e9443b825aa0bdd29b8ff671f9c4fd9b
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-19 20:35:44 +0100

    handle submit and select user

--- src/view/Chat.js
+++ src/view/Chat.js
@@ -1,36 +1,116 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { useAuth } from '../context/ChatContext';
-import { Button } from '@mui/material';
-import SideBar from '../components/chat/SideBar';
-import MessageForm from '../components/chat/MessageForm';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
-
+import SideBar from "../components/chat/SideBar";
+import MessageForm from "../components/chat/MessageForm";
+import {
+  addDoc,
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+  Timestamp,
+  where,
+} from "firebase/firestore";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import Message from "../components/chat/Message";
 
 const drawerWidth = 240;
 
-function ResponsiveDrawer(props) {
+const ResponsiveDrawer = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [chat, setChat] = useState("");
+  const [text, setText] = useState("");
+  const [img, setImg] = useState("");
+  const [msgs, setMsgs] = useState([]);
+  const user1 = auth.currentUser.uid;
 
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const {Signout} = useAuth()
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-  const handleSignout= () =>{
-    Signout()
-  }
-  
 
+  useEffect(() => {
+    const usersRef = collection(db, "users");
+    // query object
+    const q = query(usersRef, where("uid", "not-in", [user1]));
+    // execute auery
+    const subscriber = onSnapshot(q, (querySnapshot) => {
+      let users = [];
+      querySnapshot.forEach((doc) => {
+        //push data in users array
+        users.push(doc.data());
+      });
+      setUsers(users);
+    });
+    return () => subscriber();
+  }, []);
+  // select user to conversetion
+  const selectUser = (user) => {
+    setChat(user);
+    const user2 = user.uid;
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
+    const msgRef = collection(db, "message", id, "chat");
+    const q = query(msgRef, orderBy("createdAt", "asc"));
+    onSnapshot(q, (querySnapshot) => {
+      let msgs = [];
+      querySnapshot.forEach((doc) => {
+        msgs.push(doc.data());
+      });
+      setMsgs(msgs);
+    });
+  };
+
+  // handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user2 = chat.uid;
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
+    // upload image in message
+    let url;
+    if (img) {
+      const imgRef = ref(
+        storage,
+        `images/${new Date().getTime()} - ${img.name}`
+      );
+      const snap = await uploadBytes(imgRef, img);
+      const dlUrl = await getDownloadURL(ref(storage, snap.ref.fullPath));
+      url = dlUrl;
+    }
+    // create collection message and document chat
+    await addDoc(collection(db, "message", id, "chat"), {
+      text,
+      from: user1,
+      to: user2,
+      createdAt: Timestamp.fromDate(new Date()),
+      media: url || "",
+    });
+    // unread message last message
+    await setDoc(doc(db, "lastMsg", id), {
+      text,
+      from: user1,
+      to: user2,
+      createdAt: Timestamp.fromDate(new Date()),
+      media: url || "",
+      unread: true,
+    });
+    // reset data text and image
+    setText("");
+    setImg("");
+  };
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
         position="fixed"
@@ -45,12 +125,12 @@ function ResponsiveDrawer(props) {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Responsive drawer
+            {chat.name}
           </Typography>
         </Toolbar>
       </AppBar>
@@ -60,37 +140,34 @@ function ResponsiveDrawer(props) {
         aria-label="mailbox folders"
       >
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <SideBar/>
+        <SideBar users={users} selectUser={selectUser} />
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
       >
         <Toolbar />
-        <Typography paragraph>
-          Lorem 
-        </Typography>
-        <Typography paragraph>
-          Consequat 
-        </Typography>
-        <Button onClick={handleSignout}> Signout</Button>
-        <a href='/profile'>profile</a>
+        <div>
+          {msgs.length
+            ? msgs.map((msg, i) => <Message key={i} msg={msg} user1={user1} />)
+            : null}
+        </div>
+        <MessageForm
+          handleSubmit={handleSubmit}
+          text={text}
+          setText={setText}
+          setImg={setImg}
+        />
       </Box>
-      <MessageForm
-                // handleSubmit= {handleSubmit}
-                // text={text}
-                // setText={setText}
-                // setImg={setImg}
-       />
     </Box>
   );
-}
+};
 
 ResponsiveDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window: PropTypes.func,
 };
 
