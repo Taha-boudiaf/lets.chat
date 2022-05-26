b797f1ef2a5cdca7b2b707adf4c8f39f89b883df
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-26 20:36:38 +0100

    add profile path

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import { Container } from "@mui/material";
-import {BrowserRouter as Router,Routes ,Route } from "react-router-dom";
-import {HomePage,AboutPage,ContactPage, PageNoFound,LoginPage,RegisterPage,ChatPage} from './components/ListAsyncPage';
-import {  UserAuthContext } from "./context/ChatContext";
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer'
-import ProtectedRoute from './components/ProtectedRoute'
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  HomePage,
+  AboutPage,
+  ContactPage,
+  PageNoFound,
+  LoginPage,
+  RegisterPage,
+  ChatPage,
+  ProfilePage,
+} from "./components/ListAsyncPage";
+import { UserAuthContext } from "./context/ChatContext";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import ProtectedRoute from "./components/ProtectedRoute";
+import "./App.css";
 
 function App() {
-  
   return (
-    <UserAuthContext> 
+    <UserAuthContext>
       <Router>
-        <Navbar/>
-          <div className='App'>
-            <Container maxWidth="lg"> 
-              <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="/about" element={<AboutPage/>}/>
-                <Route path="/contact" element={<ContactPage/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/register" element={<RegisterPage/>}/>
-                <Route path="/chat" element={<ProtectedRoute><ChatPage/></ProtectedRoute>}/>
-                <Route path="*" element={<PageNoFound/>}/>
-              </Routes>
-            </Container> 
-          </div>
-          <Footer/>
+        <Navbar />
+        <div className="App">
+          <Container maxWidth="lg">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route
+                path="/chat"
+                element={
+                  <ProtectedRoute>
+                    <ChatPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="*" element={<PageNoFound />} />
+            </Routes>
+          </Container>
+        </div>
+        <Footer />
       </Router>
-    </UserAuthContext>  
+    </UserAuthContext>
   );
 }
 
