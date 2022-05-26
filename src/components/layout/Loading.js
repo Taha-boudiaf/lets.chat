8ccc4f3c491a29a5312954299fc01cdaa441f557
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-26 20:41:25 +0100

    edit loading css to be center

--- src/components/layout/Loading.js
+++ src/components/layout/Loading.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import loading from '../../assets/loading.gif'
+import React from "react";
+import loading from "../../assets/loading.gif";
 const Loading = () => {
   return (
     <div style={Load}>
-      <img src={loading} alt='loading'/>
+      <img src={loading} alt="loading" />
     </div>
-  )
-}
+  );
+};
 const Load = {
-  display:'flex',
-  justifyContent:'center',
-  alignSelf:'center'
-}
-export default Loading
+  display: "flex",
+  justifyContent: "center",
+  alignSelf: "center",
+  alignItems: "center",
+  height: "100vh",
+};
+export default Loading;
