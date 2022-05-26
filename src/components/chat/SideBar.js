7068dca56c184102f28e10da941b4b8798629da0
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-26 20:39:36 +0100

    add style & link to profile and logout

--- src/components/chat/SideBar.js
+++ src/components/chat/SideBar.js
@@ -1,97 +1,111 @@
-import React from 'react'
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import Toolbar from '@mui/material/Toolbar';
-import { ChatItem } from "react-chat-elements";
-import { Avatar } from "react-chat-elements";
-
-import "react-chat-elements/dist/main.css"; 
-
-
-
+import React from "react";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import { ListItem, ListItemText, Typography } from "@mui/material";
+import Users from "../chat/Users";
+import Logo from "../../assets/logo/Logo.png";
+import Img from "../../assets/svg/user.png";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 
 const drawerWidth = 240;
 
-const SideBar = (props) => {
-    const { window } = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
-    const container = window !== undefined ? () => window().document.body : undefined;
+const SideBar = ({ window, users, selectUser, user }) => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
-    const drawer = (
-        <div>
-          <Avatar
-              src={"https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"}
-              alt={"logo"}
-              size="large"
-              type="circle flexible"
+  const drawer = (
+    <div>
+      <Typography
+        component="div"
+        sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+        style={logo}
+      >
+        <a href="/">
+          <img
+            src={Logo}
+            alt="logo"
+            width="70px"
+            style={{ marginTop: "12px" }}
           />
-          <Toolbar />
-          <Divider />
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-             
-                <ChatItem
-                    key={text}
-                    avatar={"https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"}
-                    alt={"Reactjs"}
-                    title={text}
-                    subtitle={"What are you doing?"}
-                    date={new Date()}
-                    unread={index}
-                />
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      );
-      const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-      };
+        </a>
+      </Typography>
+
+      <h2>
+        {user.firstName} {user.lastName}
+      </h2>
+      <Typography style={conversation}>Conversation</Typography>
+      <Divider />
+      <List>
+        {users.map((user) => (
+          <>
+            <Users key={user.uid} user={user} selectUser={selectUser} />
+            <Divider />
+          </>
+        ))}
+      </List>
+      <div style={list}>
+        <a href="/profile" className="menu">
+          <AccountCircleOutlinedIcon />
+        </a>
+        <a href="/" className="menu">
+          <LogoutOutlinedIcon />
+        </a>
+      </div>
+    </div>
+  );
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
   return (
     <div>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
+      <Drawer
+        container={container}
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        {drawer}
+      </Drawer>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: "none", sm: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
     </div>
-  )
-}
-
-export default SideBar
+  );
+};
+const conversation = {
+  display: "flex",
+  justifyContent: "left",
+  marginLeft: "13px",
+  fontSize: "24",
+  fontWeight: "600",
+};
+const logo = {
+  marginLeft: "5%",
+  marginBottom: " 40%",
+};
+const list = {
+  position: "relative",
+  display: "flex",
+  background: "#7C8396",
+  padding: "19px 10px",
+  marginTop: "65%",
+  justifyContent: "space-between",
+};
+export default SideBar;
