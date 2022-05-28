47c34d2b1d151bd9a9eee9aa841879f564c502b6
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-28 16:53:50 +0100

    remove navbar, footer and container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Container } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   HomePage,
@@ -11,8 +10,6 @@ import {
   ProfilePage,
 } from "./components/ListAsyncPage";
 import { UserAuthContext } from "./context/ChatContext";
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
 import "./App.css";
 
@@ -20,29 +17,25 @@ function App() {
   return (
     <UserAuthContext>
       <Router>
-        <Navbar />
         <div className="App">
-          <Container maxWidth="lg">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route
-                path="/chat"
-                element={
-                  <ProtectedRoute>
-                    <ChatPage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="*" element={<PageNoFound />} />
-            </Routes>
-          </Container>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route
+              path="/chat"
+              element={
+                <ProtectedRoute>
+                  <ChatPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<PageNoFound />} />
+          </Routes>
         </div>
-        <Footer />
       </Router>
     </UserAuthContext>
   );
