83457a3a102be90dd00746b61107e1b25bba6f74
Taha-boudiaf <husseintaha168@gmail.com>
2022-05-28 16:54:59 +0100

    add navbar and footer to about component

--- src/view/About.js
+++ src/view/About.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import { Container } from "@mui/material";
+import Footer from "../components/layout/Footer";
+import Navbar from "../components/layout/Navbar";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Container maxWidth="lg">About</Container>
+      <Footer />
+    </div>
+  );
+};
 
-export default About
+export default About;
