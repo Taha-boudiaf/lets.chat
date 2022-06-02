65f0a6abb9f85564227b78b1e08f294692f14a6c
Taha-boudiaf <husseintaha168@gmail.com>
2022-06-02 18:13:41 +0100

    add 404 error

--- src/view/PageNotFound.js
+++ src/view/PageNotFound.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { Container } from "@mui/material";
-import Footer from "../components/layout/Footer";
-import Navbar from "../components/layout/Navbar";
+import { Button, Container } from "@mui/material";
+import "./notfound.css";
 
 const PageNotFound = () => {
   return (
-    <div>
-      <Navbar />
-      <Container maxWidth="lg">404 Page Not Found</Container>
-
-      <Footer />
-    </div>
+    <>
+      <Container maxWidth="lg">
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div title="404">
+            404<span> Page Not Found</span>
+          </div>
+          <Button
+            style={{
+              color: "#2F3542",
+              background: "violet",
+              padding: "15px 25px",
+              border: "none",
+              borderRadius: "4px",
+              fontSize: "1rem",
+              fontWeight: "600",
+            }}
+          >
+            Go Back
+          </Button>
+        </div>
+      </Container>
+    </>
   );
 };
 
