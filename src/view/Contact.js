8ac3e0946da6d992ec6b16225c3df68f8ffe19c4
Taha-boudiaf <husseintaha168@gmail.com>
2022-06-02 16:41:05 +0100

    create contact component

--- src/view/Contact.js
+++ src/view/Contact.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Container } from "@mui/material";
 import Footer from "../components/layout/Footer";
 import Navbar from "../components/layout/Navbar";
-
+import "./contact.css";
 const Contact = () => {
   return (
     <div>
       <Navbar />
-      <Container maxWidth="lg">Contact</Container>
+      <div class="container">
+        <form>
+          <h1>Contact Us </h1>
+          <p>
+            Feel free to contact us if you need any assistance, any help or
+            another question.
+          </p>
+          <input type="text" placeholder="Name" required autofocus />
+          <input type="email" placeholder="Email" required />
+          <input type="tel" placeholder="Mobile Number" />
+          <textarea placeholder="Message" required></textarea>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
       <Footer />
     </div>
   );
