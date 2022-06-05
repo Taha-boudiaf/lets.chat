af81038491f7f714ebf9d88dd216e3dc3691f1f2
Taha-boudiaf <husseintaha168@gmail.com>
2022-06-05 17:17:36 +0100

    add functionality to contact

--- src/view/Contact.js
+++ src/view/Contact.js
@@ -1,13 +1,46 @@
-import React from "react";
+import { doc, setDoc } from "firebase/firestore";
+import React, { useState } from "react";
 import Footer from "../components/layout/Footer";
 import Navbar from "../components/layout/Navbar";
+import { db } from "../firebase";
 import "./contact.css";
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [tel, setTel] = useState("");
+  const [loader, setLoader] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoader(true);
+
+    await setDoc(doc(db, "contacts", "contact"), {
+      name: name,
+      email: email,
+      tel: tel,
+      message: message,
+    })
+      .then(() => {
+        setLoader(false);
+        alert("Your message has been submitted👍");
+      })
+      .catch((error) => {
+        alert(error.message);
+        setLoader(false);
+      });
+
+    setName("");
+    setEmail("");
+    setMessage("");
+    setTel("");
+  };
+
   return (
     <div>
       <Navbar />
       <div className="container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>
             Contact <span style={{ color: "#5352edcc" }}>Us</span>{" "}
           </h1>
@@ -15,11 +48,38 @@ const Contact = () => {
             Feel free to contact us if you need any assistance, any help or
             another question.
           </p>
-          <input type="text" placeholder="Name" required autoFocus />
-          <input type="email" placeholder="Email" required />
-          <input type="tel" placeholder="Mobile Number" />
-          <textarea placeholder="Message" required></textarea>
-          <input type="submit" value="Submit" />
+          <input
+            type="text"
+            value={name}
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+            required
+            autoFocus
+          />
+          <input
+            type="email"
+            value={email}
+            placeholder="Email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="tel"
+            value={tel}
+            placeholder="Mobile Number"
+            onChange={(e) => setTel(e.target.value)}
+          />
+          <textarea
+            placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+          <input
+            type="submit"
+            value="Submit"
+            style={{ background: loader ? "#ccc" : " rgb(2, 2, 110)" }}
+          />
         </form>
       </div>
       <Footer />
